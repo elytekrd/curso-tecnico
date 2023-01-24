bc24d5396e593b77340bc6db38a2630c0d2daff2
AlejandroG <elytek.odoo@gmail.com>
2023-01-24 15:58:29 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'descrption': """
+        Academy Module to manage training:
+        - courses
+        - session
+        - Attendees
+    """,
+    
+    'author': 'AlejandroG',
+    
+    'website': 'https://seta.com.do',
+    
+    'category': 'training',
+    'version': ' 0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
